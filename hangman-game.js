73d2c9cf99b7142763ce4ba684a02ec56555722b
Matt Brzowski <5thhand@gmail.com>
2018-02-12 13:55:29 -0700

    include you lose message & clean code

--- hangman-game.js
+++ hangman-game.js
@@ -6,34 +6,39 @@ function startNewGame() {
   let guessedLettersCounter = 0;
   let wordToGuess = currentWordChoices[Math.floor(Math.random() * currentWordChoices.length)];
   let currentWordArray = Array(wordToGuess.length).fill("_");
-  
-  console.log(wordToGuess);
-  
+
   document.getElementById("current-word").innerHTML = currentWordArray.join(" ");
   document.getElementById("guess-count").innerHTML = guessCount;
   document.getElementById("win-count").innerHTML = winCount;
-  
-  
+  document.getElementById("guessed-letters").innerHTML = guessedLetters.join(" ");
+
   document.onkeypress = function (event) {
     console.log(event.key);
     hideInstructions();
-    if (guessedLetters.indexOf(event.key) == -1) {
+    if (guessedLetters.indexOf(event.key) == -1 && guessCount > 0) {
       updateCurrentWord(event.key);
       changeGuessCount();
       updateGuessedLetters(event.key);
       checkforWin();
     }
+    if (guessCount <= 0) {
+      if (winCount > 3) {
+        document.getElementById("game-space").innerHTML = "Wow good run!! Reload page to play again";
+      } else {
+        document.getElementById("game-space").innerHTML = "You lose. Reload page to play again";
+      }
+    }
   };
-  
+
   function hideInstructions() {
     document.getElementById("instructions").style.color = "white";
   }
-  
+
   function changeWinCount() {
     winCount++;
     document.getElementById("win-count").innerHTML = winCount;
   }
-  
+
   function updateCurrentWord(key) {
     for (var i = 0; i < wordToGuess.length; i++) {
       if (key == wordToGuess[i]) {
@@ -41,23 +46,19 @@ function startNewGame() {
         document.getElementById("current-word").innerHTML = currentWordArray.join(" ");
       }
     }
-  
-  
-  
-  
-  
   }
-  
+
   function changeGuessCount() {
     guessCount--;
     document.getElementById("guess-count").innerHTML = guessCount;
   }
-  
+
   function updateGuessedLetters(key) {
     guessedLetters[guessedLettersCounter] = key;
     guessedLettersCounter++;
     document.getElementById("guessed-letters").innerHTML = guessedLetters.join(" ");
   }
+
   function checkforWin() {
     if (currentWordArray.indexOf("_") == -1) {
       document.getElementById("instructions").style.color = "black";
