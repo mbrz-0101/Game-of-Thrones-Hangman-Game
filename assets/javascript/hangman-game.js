b157d9920e6e0706301c834c93377b05eef1e540
Matt Brzowski <5thhand@gmail.com>
2018-02-13 14:26:06 -0700

    Add pictures and random picture grabber

--- assets/javascript/hangman-game.js
+++ assets/javascript/hangman-game.js
@@ -1,20 +1,17 @@
 function startNewGame() {
   let wordChoices = ["dragon", "tyrion", "jamie", "cersi", "hodor", "bran", "arya", "lannister", "sansa", "eddard", "greyjoy", "stark", "varys", "tyrell", "tommen", "joffrey", "targaryen", "danaeris", "littlefinger", "winter", "blackwater", "ygritte", "dorne", "westoros", "whatisdeadmayneverdie", "greyworm", ""];
+  let houseChoices = ["assets/images/house-stark.png", "assets/images/house-tyrell.png", "assets/images/house-baratheon.png", "assets/images/house-greyjoy.png", "assets/images/house-martell.png", "assets/images/house-hoare.png", "assets/images/house-tully.png", "assets/images/house-targaryen.png", "assets/images/house-arryn.png", "assets/images/house-gardener.png", "assets/images/house-red-god.png", "assets/images/house-lannister.png"];
   let winCount = parseInt(document.getElementById("win-count").innerHTML) + 1;
   let guessCount = 13;
   let guessedLetters = Array(13);
   let guessedLettersCounter = 0;
   let wordToGuess = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+  let houseToDisplay = houseChoices[Math.floor(Math.random() * houseChoices.length)];
   let currentWordArray = Array(wordToGuess.length).fill("_");
 
   function hideInstructions() {
     document.getElementById("instructions").style.visibility = "hidden";
     document.getElementById("instructions").style.visibility = "hidden";
-  }
-
-  function changeWinCount() {
-    winCount++;
-    document.getElementById("win-count").innerHTML = winCount;
   }
 
   function updateCurrentWord(key) {
@@ -51,6 +48,7 @@ function startNewGame() {
   document.getElementById("guess-count").innerHTML = guessCount;
   document.getElementById("win-count").innerHTML = winCount;
   document.getElementById("guessed-letters").innerHTML = guessedLetters.join(" ");
+  document.getElementById("house-picture").src = houseToDisplay;
 
   if (winCount > 0) {
     let audio = document.createElement("audio");
@@ -62,7 +60,6 @@ function startNewGame() {
   }
 
   document.onkeypress = function (event) {
-    console.log(event.key);
     hideInstructions();
     if (guessedLetters.indexOf(event.key) == -1 && guessCount >= 0) {
       updateCurrentWord(event.key);
